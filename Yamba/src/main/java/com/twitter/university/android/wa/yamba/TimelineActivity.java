8fce054ae43b1e3b869cd45646fd7c842969f765
Wolfram Arnold <warnold@twitter.com>
2014-01-08 12:36:49 -0800

    Use custom timeline row with RelativeLayout

--- Yamba/src/main/java/com/twitter/university/android/wa/yamba/TimelineActivity.java
+++ Yamba/src/main/java/com/twitter/university/android/wa/yamba/TimelineActivity.java
@@ -1,6 +1,9 @@
 package com.twitter.university.android.wa.yamba;
 
 import android.app.Activity;
+import android.app.LoaderManager;
+import android.content.CursorLoader;
+import android.content.Loader;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.widget.ListView;
@@ -8,39 +11,59 @@ import android.widget.SimpleCursorAdapter;
 
 import com.twitter.university.android.wa.yamba.service.YambaContract;
 
-public class TimelineActivity extends Activity {
+public class TimelineActivity extends Activity implements LoaderManager.LoaderCallbacks<Cursor> {
     private static final String[] FROM = {
             YambaContract.Timeline.Columns.HANDLE,
+            YambaContract.Timeline.Columns.TIMESTAMP,
             YambaContract.Timeline.Columns.TWEET,
     };
     private static final int[] TO = {
-            android.R.id.text1,
-            android.R.id.text2,
+            R.id.text_user,
+            R.id.text_date,
+            R.id.text_tweet,
     };
+    private static final int TIMELINE_LOADER = 42;
+    private SimpleCursorAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timeline);
 
-        Cursor cursor = getContentResolver().query(
-                YambaContract.Timeline.URI,
-                null,
-                null,
-                null,
-                YambaContract.Timeline.Columns.TIMESTAMP + " DESC"
-        );
+        getLoaderManager().initLoader(TIMELINE_LOADER, null, this);
 
-        SimpleCursorAdapter adapter = new SimpleCursorAdapter(
+        mAdapter = new SimpleCursorAdapter(
                 this,
-                android.R.layout.simple_expandable_list_item_2,
-                cursor,
+                R.layout.timeline_row,
+                null,
                 FROM,
                 TO,
                 0
         );
 
         ListView listTimeline = (ListView) findViewById(R.id.list_timeline);
-        listTimeline.setAdapter(adapter);
+        listTimeline.setAdapter(mAdapter);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        return new CursorLoader(
+                getApplicationContext(),
+                YambaContract.Timeline.URI,
+                null,
+                null,
+                null,
+                YambaContract.Timeline.Columns.TIMESTAMP + " DESC"
+        );
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
+        mAdapter.swapCursor(cursor);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> cursorLoader) {
+        mAdapter.swapCursor(null);
     }
 }
